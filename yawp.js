42fd96c65a4e692a5919f08001bc99d649f7f617
halocline <mdglissmann@gmail.com>
2015-05-09 08:26:41 -0600

    Initial commit

--- yawp.js
+++ yawp.js
@@ -1,10 +1,36 @@
 // yawp.js
 
+
+/* MongoDB Models */
 Challenges = new Mongo.Collection("challenges");
 Objectives = new Mongo.Collection("objectives");
 
+/* Routes using iron:router */
+Router.map(function () {
+  this.route('home', {
+    path : '/'
+  });
+  this.route('challenge', {
+    path : '/challenge/:id',
+    data : function () {
+      var id = this.params.id;
+      return Challenges.findOne(id);
+    }
+  });
+  this.route('challenges');
+});
+
+//
+//
+/* Client scripts */
+//
+//
 if (Meteor.isClient) {
-  Template.body.events({
+  Template.body.events({});
+  Template.body.helpers({});
+
+  /* challenges */
+  Template.challenges.events({
     "submit .new-challenge" : function (event) {
       var name = event.target.challengename.value;
 
@@ -25,7 +51,7 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.body.helpers({
+  Template.challenges.helpers({
     challenges : function () {
       return Challenges.find();
     },
@@ -34,15 +60,16 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.challenge.events({
+  /* challengeItem */
+  Template.challengeItem.events({
     "click .delete" : function () {
       Meteor.call("deleteChallenge", this._id);
     },
     "submit .new-objective" : function (event) {
-      var name = event.target.text.value;
+      var objective = event.target.objective.value;
       var challengeId = event.target.challengeId.value;
 
-      Meteor.call("addObjective", name, challengeId);
+      Meteor.call("addObjective", objective, challengeId);
 
       event.target.text.value = "";           // Clears the form
 
@@ -50,26 +77,59 @@ if (Meteor.isClient) {
     }
   });
   
+  Template.challengeItem.helpers({
+    objectives : function () {
+      return Objectives.find({ challengeId : this._id });
+    }
+  });
+
+  /* challenge */
+  Template.challenge.events({});
   Template.challenge.helpers({
     objectives : function () {
       return Objectives.find({ challengeId : this._id });
     }
   });
 
+  /* objective */
   Template.objective.events({
     "click .delete" : function () {
       Meteor.call("deleteObjective", this._id);
     }
   });
   Template.objective.helpers({});
+
+  /* objectiveCard */
+  Template.objectiveCard.events({
+    "change .objectiveVal" : function (event) {
+      console.log(event);
+      var key = event.target.name;
+      var val = event.target.value;
+      var objective = event.target.closest('.update-objective').objective.value;
+      console.log(objective);
+
+      Meteor.call("updateObjective", this._id, key, val, objective);
+    }
+  });
+  Template.objectiveCard.helpers({});
 }
 
+//
+//
+/* Server scripts */
+//
+//
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
 }
 
+//
+//
+/* Meteor scripts */
+//
+//
 Meteor.methods({
   addChallenge : function (name) {
     Challenges.insert({
@@ -82,12 +142,28 @@ Meteor.methods({
 
     Challenges.remove(challenge);
   },
-  addObjective : function (name, challengeId) {
+  addObjective : function (objective, challengeId) {
     Objectives.insert({
       challengeId : challengeId,
-      name : name,
+      objective : objective,
       createdAt : new Date()
     });
+  },
+  updateObjective : function (objectiveId, key, value, obj) {
+    var objective = Objectives.findOne(objectiveId);
+    var keyval = {};
+    keyval[key] = value;
+    keyval["objective"] = obj;
+    console.log(keyval);
+    Objectives.update(
+      objective,
+      { 
+        $set : keyval,
+        $currentDate : { 
+          modifiedAt : { $type : "date" }
+        }
+      }
+    );
   },
   deleteObjective : function (objectiveId) {
     var objective = Objectives.findOne(objectiveId);
